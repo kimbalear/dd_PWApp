3a8e5963da745fe101b2cfc3364f78a0cac5a7e9
kimbalear <kimbalear@gmail.com>
2021-03-29 11:02:09 +0200

    del log

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 jQuery(document).ready(function () {
 
     $('#menu_icon').click(function () {
-        console.log(1);
         $('.ham_bar').animate({ 'left': '0' }, 500);
         $('.scrim').show();
     });
     $('.scrim').click(function () {
-        console.log(2);
         $('.ham_bar').animate({ 'left': '-228px' }, 500);
         $('.scrim').hide();
     });
-    
+
 });
